79df4dc2e4a7e70e13b0d92781f754406924c33b
hxyro <hxyro.hack@gmail.com>
2022-01-20 19:10:17 +0530

    main updated

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from libxenon import *
+from libxenon import get_messages, parse_config, is_time
 
-#functions from libxenon===>
-    #login(url,email,password)
-    #join_meet(url)
-    #print_status(Bool,sring)
-    #parse_config()
+global user, email, password, role_id, channel_id, discord_token, time_delay
 
 def main():
-    url = "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3AgidhHrv8tf9Y-NItUcNUlipQpIwYaied8nfhGzdFpYY1%40thread.tacv2%2F1642324424304%3Fcontext%3D%257b%2522Tid%2522%253a%2522161a86cc-cc0c-4622-b590-d0b92db751fe%2522%252c%2522Oid%2522%253a%2522d36a1c19-ce64-4501-9f63-6218f74c433e%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=6cc606b1-e851-4809-ab32-0f93784f9bb2&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
-
-
-#start webdriver
-    print_status(True,"Starting webdriver....")
-
-
-    ehh = parse_config()
-    if ehh == None:
-        exit
+    config = parse_config()
+    
+    if config == None:
+        return 1    
     else:
-        a,b = login(ehh['url'], ehh['email'] ,ehh['password'])
-        print_status(a,b)
-        if a:
-            join_meet(url);
+        user = config['user']
+        email = config['email']
+        password = config['password']
+        mention_role_id = config['mention_role_id']
+        channel_id = config['channel_id']
+        discord_token = config['discord_token']
+        time_delay = config['time_delay']
+    
+    while True:
+        block = get_messages(channel_id, mention_role_id, discord_token)
+        timestamp = block['timestamp']
+        time_diff = is_time(timestamp)
+        _hr = int(time_diff/60)
+        _min = int(time_diff%60) 
+        print(f"recent message:\n{block['footer']['text']}\t   tag: @IT branch")
+        print(f"\nurl: {block['url']}")
+        print(f"\ntime difference: {_hr}hour,{_min}min")
+        break
+
 
 if __name__ == "__main__":
     main()
